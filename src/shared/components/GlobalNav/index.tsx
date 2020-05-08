926438b7c5f953db2968d6ad515bcd67e0457891
Yazeed Bzadough <ybzadough@gmail.com>
2020-05-08 18:41:32 -0400

    add FirstNav icons with tooltips

--- src/shared/components/GlobalNav/index.tsx
+++ src/shared/components/GlobalNav/index.tsx
@@ -2,14 +2,44 @@ import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import FolderIcon from '@atlaskit/icon/glyph/folder';
 import PeopleIcon from '@atlaskit/icon/glyph/people';
+import QuestionCircleIcon from '@atlaskit/icon/glyph/question-circle';
 import { JiraSoftwareIcon } from '@atlaskit/logo';
+import { Tooltip } from 'react-tippy';
 import './GlobalNav.scss';
 
 export const GlobalNav: FC = () => {
   return (
     <div className="global-nav">
       <div className="first-nav">
-        <JiraSoftwareIcon />
+        <div className="jira-icon-wrapper">
+          <Tooltip position="right" title="Jira Software">
+            <JiraSoftwareIcon label="Jira Software" iconColor="inherit" />
+          </Tooltip>
+        </div>
+
+        <div className="icon-wrapper">
+          <Tooltip position="right" title="Help">
+            <QuestionCircleIcon label="Help" secondaryColor="inherit" />
+          </Tooltip>
+        </div>
+
+        <div className="icon-wrapper">
+          <Tooltip position="right" title="TODO: Switch to">
+            <QuestionCircleIcon
+              label="TODO: Switch to"
+              secondaryColor="inherit"
+            />
+          </Tooltip>
+        </div>
+
+        <div className="icon-wrapper">
+          <Tooltip position="right" title="TODO: Your profile and settings">
+            <QuestionCircleIcon
+              label="TODO: Your profile and settings"
+              secondaryColor="inherit"
+            />
+          </Tooltip>
+        </div>
       </div>
 
       <div className="second-nav">
