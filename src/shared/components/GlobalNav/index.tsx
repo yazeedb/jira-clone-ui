8906abf9c881624430072a507037b986095549d4
Yazeed Bzadough <ybzadough@gmail.com>
2020-05-09 23:18:31 -0400

    Squashed commit of the following:
    commit 53a66357f759a5de320ee0a6ba4202e13862c5e0
    Author: Yazeed Bzadough <ybzadough@gmail.com>
    Date:   Sat May 9 23:18:20 2020 -0400
    
        GlobalNav: Add collapse

--- src/shared/components/GlobalNav/index.tsx
+++ src/shared/components/GlobalNav/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import FolderIcon from '@atlaskit/icon/glyph/folder';
 import PeopleIcon from '@atlaskit/icon/glyph/people';
@@ -8,6 +8,8 @@ import StarLargeIcon from '@atlaskit/icon/glyph/star-large';
 import SearchIcon from '@atlaskit/icon/glyph/search';
 import AddIcon from '@atlaskit/icon/glyph/add';
 import NotificationIcon from '@atlaskit/icon/glyph/notification';
+import ChevronLeftIcon from '@atlaskit/icon/glyph/chevron-left';
+import ChevronRightIcon from '@atlaskit/icon/glyph/chevron-right';
 import { JiraSoftwareIcon } from '@atlaskit/logo';
 import { Tooltip } from 'react-tippy';
 import './GlobalNav.scss';
@@ -16,6 +18,8 @@ const mockProfilePicture =
   'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/5ac56eefa05a8d1c98846c61/fe6f2859-d1ac-4e9e-a982-29214fd34c37/128?size=48&s=48';
 
 export const GlobalNav: FC = () => {
+  const [secondNavOpen, setSecondNavOpen] = useState(true);
+
   return (
     <div className="global-nav">
       <div className="first-nav">
@@ -85,7 +89,7 @@ export const GlobalNav: FC = () => {
         </div>
       </div>
 
-      <div className="second-nav">
+      <div className={secondNavOpen ? 'second-nav open' : 'second-nav closed'}>
         <h1 className="title">Jira Software</h1>
         <ul>
           <li>
@@ -101,6 +105,21 @@ export const GlobalNav: FC = () => {
             </Link>
           </li>
         </ul>
+
+        <button
+          className="toggle-second-nav"
+          onClick={() => setSecondNavOpen((v) => !v)}
+        >
+          {secondNavOpen ? (
+            <Tooltip position="right" title="Collapse">
+              <ChevronLeftIcon label="Collapse" secondaryColor="inherit" />
+            </Tooltip>
+          ) : (
+            <Tooltip position="right" title="Expand">
+              <ChevronRightIcon label="Expand" secondaryColor="inherit" />
+            </Tooltip>
+          )}
+        </button>
       </div>
     </div>
   );
