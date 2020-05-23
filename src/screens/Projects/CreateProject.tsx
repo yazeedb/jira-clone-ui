2e330b3d08fc067dec77151b68e05a82122d9425
Yazeed Bzadough <ybzadough@gmail.com>
2020-05-22 20:09:38 -0400

    Extract CreateProject component

---
+++ src/screens/Projects/CreateProject.tsx
@@ -0,0 +1,123 @@
+import React, { FC, useState } from 'react';
+import Popup from '@atlaskit/popup';
+import Drawer from '@atlaskit/drawer';
+import Form, { Field } from '@atlaskit/form';
+import TextField from '@atlaskit/textfield';
+import InfoIcon from '@atlaskit/icon/glyph/info';
+import Button from '@atlaskit/button';
+import './CreateProject.scss';
+
+interface CreateProjectProps {
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+export const CreateProject: FC<CreateProjectProps> = ({ onClose, isOpen }) => {
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  return (
+    <Drawer width="full" onClose={onClose} isOpen={isOpen}>
+      <div className="create-projects">
+        <Form onSubmit={(data) => console.log('form data', data)}>
+          {({ formProps }) => (
+            <form {...formProps}>
+              <h1 className="title">Create project</h1>
+
+              <Field name="projectName" defaultValue="" label="Name">
+                {({ fieldProps }) => (
+                  <TextField
+                    placeholder="Enter a project name"
+                    autoFocus
+                    {...fieldProps}
+                  />
+                )}
+              </Field>
+
+              <Field name="projectKey" defaultValue="" label="Key">
+                {({ fieldProps }) => (
+                  <div className="flex-wrapper">
+                    <TextField {...fieldProps} />
+
+                    <Popup
+                      isOpen={popupOpen}
+                      placement="right"
+                      onClose={() => setPopupOpen(false)}
+                      content={() => {
+                        return (
+                          <div className="popup-wrapper">
+                            <p>
+                              The project key is used as the prefix of your
+                              project's issue keys (e.g. 'TEST-100'). Choose one
+                              that is descriptive and easy to type.
+                            </p>
+
+                            <a
+                              href="https://support.atlassian.com/jira-core-cloud/docs/work-with-issues-in-jira-cloud/"
+                              target="_blank"
+                              rel="noreferrer noopener"
+                            >
+                              Learn more
+                            </a>
+                          </div>
+                        );
+                      }}
+                      trigger={(triggerProps) => {
+                        return (
+                          <Button
+                            className="popup-trigger"
+                            style={{
+                              marginLeft: '10px',
+                              cursor: 'pointer',
+                              background: 'none'
+                            }}
+                            onClick={() => setPopupOpen((v) => !v)}
+                            {...triggerProps}
+                          >
+                            <InfoIcon
+                              label="More info"
+                              primaryColor="rgb(101, 84, 192)"
+                              size="medium"
+                            />
+                          </Button>
+                        );
+                      }}
+                      zIndex={999} // Otherwise it won't show above the Drawer -_-"
+                    />
+                  </div>
+                )}
+              </Field>
+
+              <Field name="template" defaultValue="kanban" label="Template">
+                {() => (
+                  <div className="template-wrapper">
+                    <img src="kanban-template.svg" alt="Kanban template" />
+
+                    <div>
+                      <h4>Kanban</h4>
+                      <p>
+                        Visualize and progress your project using simple cards
+                        on a powerful board
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </Field>
+
+              <Button
+                type="submit"
+                appearance="primary"
+                shouldFitContainer
+                style={{
+                  marginTop: '25px',
+                  height: '40px'
+                }}
+              >
+                Create
+              </Button>
+            </form>
+          )}
+        </Form>
+      </div>
+    </Drawer>
+  );
+};
--- src/screens/Projects/index.tsx
+++ src/screens/Projects/index.tsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { projectsMachine } from 'machines/projectsMachine';
 import { useMachine } from '@xstate/react';
 import Button from '@atlaskit/button';
 import ProgressBar from '@atlaskit/progress-bar';
-import Popup from '@atlaskit/popup';
-import Drawer from '@atlaskit/drawer';
-import Form, { Field } from '@atlaskit/form';
-import TextField from '@atlaskit/textfield';
-import InfoIcon from '@atlaskit/icon/glyph/info';
 import { SomethingWentWrong } from 'shared/components/SomethingWentWrong';
 import { ImpossibleStateNotice } from 'shared/components/ImpossibleStateNotice';
+import { CreateProject } from './CreateProject';
 
 export const Projects = () => {
   const [current, send] = useMachine(projectsMachine);
@@ -53,162 +49,15 @@ export const Projects = () => {
       return projects.map((p) => JSON.stringify(p));
     };
 
-    const [open, setOpen] = useState(false);
-
     return (
       <section className="view-projects">
         <h2 className="title">Projects</h2>
         {renderContent()}
 
-        <Drawer
-          onClose={() => send('CLOSE')}
+        <CreateProject
           isOpen={current.matches('viewingProjects.creatingProject')}
-          width="full"
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              height: '100%'
-            }}
-          >
-            <Form onSubmit={(data) => console.log('form data', data)}>
-              {({ formProps }) => (
-                <form
-                  style={{
-                    width: '350px',
-                    margin: '0 auto'
-                  }}
-                  {...formProps}
-                >
-                  <h1
-                    style={{
-                      fontWeight: 'normal',
-                      marginBottom: '20px'
-                    }}
-                  >
-                    Create project
-                  </h1>
-
-                  <Field name="projectName" defaultValue="" label="Name">
-                    {({ fieldProps }) => (
-                      <TextField
-                        placeholder="Enter a project name"
-                        autoFocus
-                        {...fieldProps}
-                      />
-                    )}
-                  </Field>
-
-                  <Field name="projectKey" defaultValue="" label="Key">
-                    {({ fieldProps }) => (
-                      <div
-                        style={{
-                          display: 'flex',
-                          alignContent: 'row',
-                          alignItems: 'center',
-                          width: '180px'
-                        }}
-                      >
-                        <TextField {...fieldProps} />
-
-                        <Popup
-                          isOpen={open}
-                          placement="right"
-                          onClose={() => setOpen(false)}
-                          zIndex={9999}
-                          content={() => {
-                            return (
-                              <div
-                                style={{
-                                  width: '300px',
-                                  padding: '16px 24px'
-                                }}
-                              >
-                                <p style={{ marginBottom: '10px' }}>
-                                  The project key is used as the prefix of your
-                                  project's issue keys (e.g. 'TEST-100'). Choose
-                                  one that is descriptive and easy to type.
-                                </p>
-
-                                <a
-                                  href="https://support.atlassian.com/jira-core-cloud/docs/work-with-issues-in-jira-cloud/"
-                                  target="_blank"
-                                  rel="noreferrer noopener"
-                                >
-                                  Learn more
-                                </a>
-                              </div>
-                            );
-                          }}
-                          trigger={(triggerProps) => {
-                            return (
-                              // @ts-ignore
-                              <div
-                                style={{
-                                  marginLeft: '10px',
-                                  cursor: 'pointer'
-                                }}
-                                onClick={() => setOpen((v) => !v)}
-                                {...triggerProps}
-                              >
-                                <InfoIcon
-                                  label="More info"
-                                  primaryColor="rgb(101, 84, 192)"
-                                  size="medium"
-                                />
-                              </div>
-                            );
-                          }}
-                        />
-                      </div>
-                    )}
-                  </Field>
-
-                  <Field name="template" defaultValue="kanban" label="Template">
-                    {() => (
-                      <div
-                        style={{
-                          display: 'flex',
-                          justifyContent: 'space-between'
-                        }}
-                      >
-                        <img
-                          src="kanban-template.svg"
-                          alt="Kanban template"
-                          style={{
-                            width: '150px',
-                            marginRight: '10px'
-                          }}
-                        />
-
-                        <div>
-                          <h4>Kanban</h4>
-                          <p style={{ color: 'rgb(107, 119, 140)' }}>
-                            Visualize and progress your project using simple
-                            cards on a powerful board
-                          </p>
-                        </div>
-                      </div>
-                    )}
-                  </Field>
-
-                  <Button
-                    type="submit"
-                    appearance="primary"
-                    shouldFitContainer
-                    style={{
-                      marginTop: '25px',
-                      height: '40px'
-                    }}
-                  >
-                    Create
-                  </Button>
-                </form>
-              )}
-            </Form>
-          </div>
-        </Drawer>
+          onClose={() => send('CLOSE')}
+        />
       </section>
     );
   }
