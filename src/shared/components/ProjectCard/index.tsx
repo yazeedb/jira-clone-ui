5d416c6c52fe91d193598bb3a5ca543c02708429
Yazeed Bzadough <ybzadough@gmail.com>
2020-05-28 17:40:27 -0400

    add project cards

--- src/shared/components/ProjectCard/index.tsx
+++ src/shared/components/ProjectCard/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import BoardIcon from '@atlaskit/icon/glyph/board';
 import { Project } from 'shared/interfaces/Project';
+import './ProjectCard.scss';
 
 interface ProjectCardProps {
   project: Project;
@@ -8,50 +9,23 @@ interface ProjectCardProps {
 
 export const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
   return (
-    <section
-      style={{
-        maxWidth: '260px',
-        display: 'flex',
-        flexDirection: 'column',
-        borderRadius: '3px',
-        boxShadow:
-          'rgba(9, 30, 66, 0.25) 0px 1px 1px, rgba(9, 30, 66, 0.31) 0px 0px 1px'
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          padding: '30px 18px',
-          backgroundColor: 'rgb(179, 212, 255)'
-        }}
-      >
+    <section className="project-card">
+      <header>
         <img
+          className="project-avatar"
           src="mockProjectAvatar.svg"
           alt={project.name}
-          style={{
-            width: '40px',
-            height: '40px',
-            marginRight: '12px',
-            borderRadius: '3px'
-          }}
         />
 
         <div>
           <span>{project.name}</span>
-          <p style={{ margin: '0px' }}>{project.type}</p>
+          <p className="project-type">{project.type}</p>
         </div>
-      </div>
+      </header>
 
-      <footer
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: '10px 18px'
-        }}
-      >
-        <BoardIcon label="board" />
-        <span style={{ marginLeft: '5px' }}>{project.key} board</span>
+      <footer>
+        <BoardIcon label="board" size="small" />
+        <p className="project-key">{project.key} board</p>
       </footer>
     </section>
   );
