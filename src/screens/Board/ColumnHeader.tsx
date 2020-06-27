7318e803d3fec15234812552629178406326bae7
Yazeed Bzadough <ybzadough@gmail.com>
2020-06-27 13:51:13 -0400

    Squashed commit of the following:
    commit 44cf87001b11f7c109b263c70778f4e4262cc0c3
    Author: Yazeed Bzadough <ybzadough@gmail.com>
    Date:   Sat Jun 27 13:51:04 2020 -0400
    
        use InlineEdit
    
    commit 6f7ef2a7e50a6d72218543aac3dd9ecb75d2aa6f
    Author: Yazeed Bzadough <ybzadough@gmail.com>
    Date:   Wed Jun 24 20:16:24 2020 -0400
    
        install package

--- src/screens/Board/ColumnHeader.tsx
+++ src/screens/Board/ColumnHeader.tsx
@@ -1,12 +1,12 @@
 import React, { FC, useState } from 'react';
 import CheckIcon from '@atlaskit/icon/glyph/check';
-import CrossIcon from '@atlaskit/icon/glyph/cross';
 import { Column } from 'shared/interfaces/Project';
 import { ButtonItem, Section } from '@atlaskit/menu';
 import MoreIcon from '@atlaskit/icon/glyph/editor/more';
 import Popup from '@atlaskit/popup';
 import Button from '@atlaskit/button';
 import TextField from '@atlaskit/textfield';
+import InlineEdit from '@atlaskit/inline-edit';
 
 interface ColumnHeaderProps {
   column: Column;
@@ -18,92 +18,46 @@ interface ColumnHeaderProps {
 export const ColumnHeader: FC<ColumnHeaderProps> = ({
   column,
   showCheckmark,
-  onChange,
-  onChangeCancel
+  onChange
 }) => {
   const [showPopup, setShowPopup] = useState(false);
-  const [editingHeader, setEditingHeader] = useState(false);
-
-  const [name, setName] = useState(column.name);
-  const resetName = () => setName(column.name);
-
-  const cancelChanges = () => {
-    setEditingHeader(false);
-    resetName();
-    onChangeCancel();
-  };
 
   return (
     <header>
-      {editingHeader ? (
-        <form
-          className="edit-column-name"
-          onSubmit={(event) => {
-            event.preventDefault();
-            onChange(name);
-          }}
-        >
-          <TextField
-            autoFocus
-            onFocus={(event) => event.target.select()}
-            onBlur={cancelChanges}
-            value={name}
-            onChange={(event) => setName(event.currentTarget.value)}
-            onKeyDown={(event) => {
-              if (event.key === 'Escape') {
-                cancelChanges();
-              }
-            }}
-          />
+      <InlineEdit
+        defaultValue={column.name}
+        onConfirm={onChange}
+        editView={(fieldProps) => <TextField {...fieldProps} autoFocus />}
+        isRequired
+        readView={() => <h6 className="title">{column.name}</h6>}
+      />
 
-          <div className="form-controls">
-            <Button
-              type="submit"
-              iconBefore={<CheckIcon size="small" label="Submit" />}
-            />
-
-            <Button
-              onClick={cancelChanges}
-              iconBefore={<CrossIcon size="small" label="Cancel" />}
-            />
-          </div>
-        </form>
-      ) : (
-        <>
-          <div className="title-wrapper">
-            <Button appearance="subtle" onClick={() => setEditingHeader(true)}>
-              {column.name}
-            </Button>
-
-            {showCheckmark && (
-              <span className="check-icon">
-                <CheckIcon label="check" />
-              </span>
-            )}
-          </div>
-
-          <Popup
-            isOpen={showPopup}
-            onClose={() => setShowPopup(false)}
-            placement="bottom-end"
-            content={() => (
-              <Section>
-                <ButtonItem>Set column limit</ButtonItem>
-                <ButtonItem>Delete</ButtonItem>
-              </Section>
-            )}
-            trigger={(triggerProps) => (
-              <Button
-                {...triggerProps}
-                isSelected={showPopup}
-                onClick={() => setShowPopup(true)}
-                className={showPopup ? 'more-icon show' : 'more-icon'}
-                iconBefore={<MoreIcon size="large" label="More" />}
-              />
-            )}
-          />
-        </>
+      {showCheckmark && (
+        <span className="check-icon">
+          <CheckIcon label="check" />
+        </span>
       )}
+
+      <Popup
+        isOpen={showPopup}
+        onClose={() => setShowPopup(false)}
+        placement="bottom-end"
+        content={() => (
+          <Section>
+            <ButtonItem>Set column limit</ButtonItem>
+            <ButtonItem>Delete</ButtonItem>
+          </Section>
+        )}
+        trigger={(triggerProps) => (
+          <Button
+            {...triggerProps}
+            isSelected={showPopup}
+            onClick={() => setShowPopup(true)}
+            className={showPopup ? 'more-icon show' : 'more-icon'}
+            iconBefore={<MoreIcon size="large" label="More" />}
+          />
+        )}
+      />
     </header>
   );
 };
