efb9343a9c2af1ccec31e0571eea816adaf6d41c
Yazeed Bzadough <ybzadough@gmail.com>
2020-07-22 20:52:18 -0400

    add Task styling

--- src/shared/components/Task/index.tsx
+++ src/shared/components/Task/index.tsx
@@ -1,4 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import MoreIcon from '@atlaskit/icon/glyph/editor/more';
+import Button from '@atlaskit/button';
+import { ButtonItem, Section } from '@atlaskit/menu';
+import Popup from '@atlaskit/popup';
 import { Task } from 'shared/interfaces/Project';
 import './Task.scss';
 
@@ -7,9 +11,55 @@ interface TaskProps {
 }
 
 export const TaskComponent: FC<TaskProps> = ({ task }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const closePopup = () => setShowPopup(false);
+  const openPopup = () => setShowPopup(true);
+
   return (
     <div className="task">
-      <span>{task.name}</span>
+      <header>
+        <span>{task.name}</span>
+
+        <Popup
+          isOpen={showPopup}
+          onClose={closePopup}
+          placement="bottom-end"
+          content={() => (
+            <Section>
+              <ButtonItem
+                onClick={() => {
+                  closePopup();
+                }}
+              >
+                Copy issue link
+              </ButtonItem>
+
+              <ButtonItem
+                onClick={() => {
+                  closePopup();
+                }}
+              >
+                Delete
+              </ButtonItem>
+            </Section>
+          )}
+          trigger={(triggerProps) => (
+            <Button
+              {...triggerProps}
+              isSelected={showPopup}
+              onClick={showPopup ? closePopup : openPopup}
+              className={showPopup ? 'more-actions show' : 'more-actions hide'}
+              iconBefore={<MoreIcon size="large" label="More" />}
+            />
+          )}
+        />
+      </header>
+
+      <footer>
+        <img src="/task-icon.svg" />
+        <span className="ui-sequence">FP-13</span>
+      </footer>
     </div>
   );
 };
