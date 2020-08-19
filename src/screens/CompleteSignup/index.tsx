3dbb5fdd56d91f92a6ec84e6997c68948d262bec
Yazeed Bzadough <ybzadough@gmail.com>
2020-08-19 01:14:37 -0400

    remove excess fragment

--- src/screens/CompleteSignup/index.tsx
+++ src/screens/CompleteSignup/index.tsx
@@ -30,70 +30,68 @@ export const CompleteSignup: FC<CompleteSignupProps> = ({
   const fieldsWithEmptyStringDefaults = mapObject((value) => value || '', user);
 
   return (
-    <>
-      <LandingForm containerClassName="complete-signup">
-        <h4 className="heading">Complete your profile to continue</h4>
+    <LandingForm containerClassName="complete-signup">
+      <h4 className="heading">Complete your profile to continue</h4>
 
-        <Formik
-          initialValues={fieldsWithEmptyStringDefaults}
-          validate={validateForm}
-          onSubmit={(values) => {
-            send({
-              type: 'SUBMIT',
-              formData: values
-            });
-          }}
-        >
-          {({ isValid }) => (
-            <Form>
-              <Field type="email" name="email" disabled />
+      <Formik
+        initialValues={fieldsWithEmptyStringDefaults}
+        validate={validateForm}
+        onSubmit={(values) => {
+          send({
+            type: 'SUBMIT',
+            formData: values
+          });
+        }}
+      >
+        {({ isValid }) => (
+          <Form>
+            <Field type="email" name="email" disabled />
 
-              <FormField
-                name="firstName"
-                type="text"
-                placeholder="First name"
-                autoFocus
-              />
+            <FormField
+              name="firstName"
+              type="text"
+              placeholder="First name"
+              autoFocus
+            />
 
-              <FormField name="lastName" placeholder="Last name" type="text" />
+            <FormField name="lastName" placeholder="Last name" type="text" />
 
-              <FormField
-                type="text"
-                placeholder="Job title (optional)"
-                name="jobTitle"
-              />
+            <FormField
+              type="text"
+              placeholder="Job title (optional)"
+              name="jobTitle"
+            />
 
-              <FormField
-                type="text"
-                placeholder="Department (optional)"
-                name="department"
-              />
+            <FormField
+              type="text"
+              placeholder="Department (optional)"
+              name="department"
+            />
 
-              <FormField
-                type="text"
-                placeholder="Organization (optional)"
-                name="organization"
-              />
+            <FormField
+              type="text"
+              placeholder="Organization (optional)"
+              name="organization"
+            />
 
-              <FormField
-                type="text"
-                placeholder="Location (optional)"
-                name="location"
-              />
+            <FormField
+              type="text"
+              placeholder="Location (optional)"
+              name="location"
+            />
 
-              <Button
-                type="submit"
-                isLoading={current.matches(SignupStates.submitting)}
-                isDisabled={!isValid}
-                appearance="primary"
-                shouldFitContainer
-              >
-                Complete profile
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </LandingForm>
-    </>
+            <Button
+              type="submit"
+              isLoading={current.matches(SignupStates.submitting)}
+              isDisabled={!isValid}
+              appearance="primary"
+              shouldFitContainer
+            >
+              Complete profile
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </LandingForm>
   );
 };
